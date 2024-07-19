325dd77e19d8cd298368c5d606dd0d3c54f559fb
Dukyoon Im <DUKY8N@gmail.com>
2024-07-20 00:55:46 +0900

    로그인, 로그아웃, 토큰 재발행 API 수정

--- src/apis/api/renewRefreshToken.js
+++ src/apis/api/renewRefreshToken.js
@@ -4,23 +4,18 @@ const baseURL = 'http://minnnisu.iptime.org';
 
 /**
  * 서버에 액세스 토큰과 리프레시 토큰을 이용하여 새로운 액세스 토큰을 요청합니다.
+ * return값이 없고 성공적으로 토큰을 갱신한 경우 localStorage에 새로운 액세스 토큰을 저장합니다.
  *
  * @returns {Promise<void>} 성공적으로 토큰을 갱신했을 경우 아무 값도 반환하지 않습니다.
  * @throws {HttpResponseError} 토큰 갱신 요청에 실패하거나 서버로부터 예상치 못한 응답을 받았을 경우 HttpResponseError를 발생시킵니다.
- *
- * 이 함수는 서버의 "/api/auth/refreshToken" 엔드포인트에 POST 요청을 보내어
- * 새로운 액세스 토큰을 받습니다. 요청은 로컬 스토리지에 저장된 액세스 토큰과 리프레시 토큰을
- * 헤더에 포함하여 보내며, 서버로부터 받은 새로운 액세스 토큰은 로컬 스토리지에 저장합니다.
- * 응답이 정상적이지 않을 경우, 서버에서 받은 에러 메시지와 상태 코드를
- * 사용하여 HttpResponseError 예외를 생성하고 던집니다.
  */
 const renewRefreshToken = async () => {
   const response = await fetch(`${baseURL}/api/auth/refreshToken`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: localStorage.getItem('accessToken'),
-      'Authorization-refresh': localStorage.getItem('refreshToken'),
+      Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      'Authorization-refresh': `Bearer ${localStorage.getItem('refreshToken')}`,
     },
   });
   const result = await response.json();
--- src/apis/api/submitSignin.js
+++ src/apis/api/submitSignin.js
@@ -10,11 +10,6 @@ const baseURL = 'http://minnnisu.iptime.org';
  * @returns {Promise<void>} 로그인이 성공적으로 완료되면 아무 값도 반환하지 않습니다.
  * @throws {UserNotFoundError} 제공된 사용자 정보가 서버에 존재하지 않을 경우 발생합니다.
  * @throws {HttpResponseError} 로그인 요청이 실패하거나 서버로부터 예상치 못한 응답을 받았을 경우 발생합니다.
- *
- * 이 함수는 먼저 `validateParameters` 함수를 호출하여 필요한 파라미터가 모두 있는지 확인합니다.
- * 이후 "/api/auth/login" 엔드포인트에 POST 요청을 보내 로그인을 시도하고, 서버로부터 받은
- * 응답을 파싱하여 필요한 정보(액세스 토큰, 리프레시 토큰, 사용자 타입)를 로컬 스토리지에 저장합니다.
- * 응답이 정상적이지 않을 경우, 서버에서 받은 에러 타입에 따라 적절한 예외를 발생시킵니다.
  */
 const submitSignin = async (params) => {
   validateParameters(params, ['username', 'password']);

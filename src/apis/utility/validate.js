1f81aa0a13c0152f74521c9c28d62da8c48d2a41
Dukyoon Im <DUKY8N@gmail.com>
2024-07-20 00:58:07 +0900

    객체에 키가 있어도 값이 nullish하면 error 발생 수정

--- src/apis/utility/validate.js
+++ src/apis/utility/validate.js
@@ -1,7 +1,13 @@
 import { ParametersValidationError } from './errors.js';
 
 export const validateParameters = (params, requiredFields = []) => {
-  const missingFields = requiredFields.filter((field) => !params[field]);
+  const missingFields = requiredFields.filter((field) => !(field in params));
 
   if (missingFields.length > 0) throw new ParametersValidationError(missingFields);
 };
+
+// export const validateParameters = (params, requiredFields = []) => {
+//   const missingFields = requiredFields.filter((field) => !params[field]);
+
+//   if (missingFields.length > 0) throw new ParametersValidationError(missingFields);
+// };

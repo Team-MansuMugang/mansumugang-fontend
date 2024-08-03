3d8639a84372b0ed61876848ac1b233ac1ee50a6
Dukyoon <DUKY8N@gmail.com>
2024-08-04 03:51:56 +0900

    오탈자 수정

--- src/apis/api/checkProtectorUsername.js
+++ src/apis/api/checkProtectorUsername.js
@@ -6,10 +6,10 @@ const baseURL = 'http://minnnisu.iptime.org';
  * 보호자 사용자 ID가 존재하는지 확인합니다.
  *
  * @async
- * @function checkUsernameUnique
+ * @function checkProtectorUsername
  * @param {string} username - 확인할 사용자 이름.
  * @returns {Promise<boolean>} - 사용자 이름이 고유하면 true, 그렇지 않으면 false를 반환합니다.
- * @throws {HttpResponseError} - 요청이 실패하고 'NotValidRequestError' 또는 'UserTypeDismatchError'가 아닌 경우 발생합니다.
+ * @throws {HttpResponseError} - 서버로부터 예상치 못한 응답이나 에러 응답을 받았을 경우 HttpResponseError를 발생시킵니다.
  */
 const checkProtectorUsername = async (username) => {
   const response = await fetch(`${baseURL}/api/auth/check/protectorUsername`, {

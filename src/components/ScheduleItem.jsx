ba07b8dd69a5165d38c4b0cd0d15a7304366170b
Dukyoon Im <DUKY8N@gmail.com>
2024-08-12 09:55:18 +0900

    코드 리팩터링

--- src/components/ScheduleItem.jsx
+++ src/components/ScheduleItem.jsx
@@ -1,25 +1,14 @@
-import PropTypes from 'prop-types';
 import './ScheduleItem.css';
 import CheckCircle from '../assets/svg/check-circle-outline.svg?react';
 
 const ScheduleItem = ({ status, title, data }) => {
   return (
     <div className="schedule-item">
-      <div className={`schedule-status schedule-status-${status}`}>
-        {status === 'checked' ? <CheckCircle /> : <CheckCircle />}
-      </div>
-      <div className="schedule-text">
-        <div className="schedule-title">{title}</div>
-        <div className="schedule-data">{data}</div>
-      </div>
+      <CheckCircle className={`status-${status}`} />
+      <div className="schedule-title">{title}</div>
+      <div className="schedule-data">{data}</div>
     </div>
   );
-};
-
-ScheduleItem.propTypes = {
-  status: PropTypes.oneOf(['checked', 'unchecked']).isRequired,
-  title: PropTypes.string.isRequired,
-  data: PropTypes.string.isRequired,
 };
 
 export default ScheduleItem;

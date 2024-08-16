a53d38d7a44a3a44b22e227df719b99f2ad12f99
Dukyoon Im <DUKY8N@gmail.com>
2024-08-16 17:00:23 +0900

    onClick 콜백함수 매개변수 추가

--- src/components/ScheduleItem.jsx
+++ src/components/ScheduleItem.jsx
@@ -1,9 +1,9 @@
 import './ScheduleItem.css';
 import CheckCircle from '../assets/svg/check-circle-outline.svg?react';
 
-const ScheduleItem = ({ status, title, data }) => {
+const ScheduleItem = ({ status, title, data, onClick }) => {
   return (
-    <div className="schedule-item">
+    <div className="schedule-item" onClick={onClick}>
       <CheckCircle className={`status-${status}`} />
       <div className="schedule-title">{title}</div>
       <div className="schedule-data">{data}</div>

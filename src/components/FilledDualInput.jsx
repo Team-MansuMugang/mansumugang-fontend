7ae65eb7f1abcbfbf8d0fd9a52e8d706addcc082
Dukyoon Im <DUKY8N@gmail.com>
2024-08-20 01:28:02 +0900

    HospitalAddPage 카카오맵 api 반영 및 폼 상태 처리

--- src/components/FilledDualInput.jsx
+++ src/components/FilledDualInput.jsx
@@ -6,7 +6,9 @@ import '../index.css';
  * @typedef {Object} Props
  * @property {string[]} [placeholder=['입력칸 1', '입력칸 2']] - 두 개의 입력 필드에 표시할 자리 표시자 텍스트 배열
  * @property {function(string, string): void} [onInputChange] - 입력 필드 값이 변경될 때 호출되는 콜백 함수. 첫 번째 인자는 입력 필드의 name, 두 번째 인자는 입력된 value
+ * @property {function(string): void} [onInputClick] - 입력 필드가 클릭될 때 호출되는 콜백 함수. 인자는 클릭된 입력 필드의 name
  * @property {string[]} [init=[]] - 각 입력 필드의 초기값을 설정하기 위한 배열
+ * @property {boolean[]} [viewOnly=[false, false]] - 각 입력 필드를 읽기 전용으로 설정하기 위한 배열
  */
 
 /**
@@ -17,6 +19,8 @@ const FilledDualInput = ({
   placeholder = ['입력칸 1', '입력칸 2'],
   init = ['', ''],
   onInputChange,
+  onInputClick,
+  viewOnly = [false, false],
 }) => {
   const [input1, setInput1] = useState(init[0]);
   const [input2, setInput2] = useState(init[1]);
@@ -34,6 +38,11 @@ const FilledDualInput = ({
     if (onInputChange) onInputChange(name, value);
   };
 
+  const handleClick = (event) => {
+    const { name } = event.target;
+    if (onInputClick) onInputClick(name);
+  };
+
   return (
     <div className="dual-input">
       <input
@@ -42,6 +51,9 @@ const FilledDualInput = ({
         placeholder={placeholder[0]}
         value={input1}
         onChange={handleChange}
+        onClick={handleClick}
+        readOnly={viewOnly[0]}
+        className={`second-input ${viewOnly[0] ? 'view-only' : ''}`}
       />
       <input
         type="text"
@@ -49,6 +61,9 @@ const FilledDualInput = ({
         placeholder={placeholder[1]}
         value={input2}
         onChange={handleChange}
+        onClick={handleClick}
+        readOnly={viewOnly[1]}
+        className={`second-input ${viewOnly[1] ? 'view-only' : ''}`}
       />
     </div>
   );
--- src/pages/schedule/HospitalAddPage.jsx
+++ src/pages/schedule/HospitalAddPage.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import DaumPostcode from 'react-daum-postcode';
 import './HospitalEditPage.css';
 import '../../index.css';
 import MainHeader from '../../components/MainHeader';
@@ -11,32 +13,137 @@ import FilledDateInput from '../../components/FilledDateInput';
 
 const HospitalAddPage = () => {
   const navigate = useNavigate();
+
+  const [hospitalName, setHospitalName] = useState('');
+  const [hospitalAddress, setHospitalAddress] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [hospitalDescription, setHospitalDescription] = useState('');
+  const [inputHospitalVisitDate, setInputHospitalVisitDate] = useState({
+    year: '',
+    month: '',
+    day: '',
+  });
+  const [inputHospitalVisitTime, setInputHospitalVisitTime] = useState({
+    hours: '',
+    minutes: '',
+    meridiem: 'AM',
+  });
+  const [isPostcodeOpen, setIsPostcodeOpen] = useState(false);
+
+  useEffect(() => {
+    var geocoder = new kakao.maps.services.Geocoder();
+
+    var callback = function (result, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        setLatitude(result[0].x);
+        setLongitude(result[0].y);
+      }
+    };
+
+    if (hospitalAddress) geocoder.addressSearch(hospitalAddress, callback);
+  }, [hospitalAddress]);
+
+  const handleAddressSelection = (data) => {
+    let fullAddress = data.address;
+    setHospitalAddress(fullAddress);
+    setIsPostcodeOpen(false);
+  };
+
+  const handleInputClick = (inputIndex) => {
+    if (inputIndex === 'input2') {
+      setIsPostcodeOpen(true);
+    }
+  };
+
+  const handlePostcodeWrapClick = () => {
+    setIsPostcodeOpen(false);
+  };
+
   return (
     <div className="hospital-edit-page">
       <MainHeader title="병원 일정 추가 페이지" onClickLeft={() => navigate(-1)} />
       <div className="contents">
         <div className="top-container">
-          <FilledDualInput placeholder={['병원 이름', '병원 주소']} />
+          <FilledDualInput
+            placeholder={['병원 이름', '병원 주소']}
+            viewOnly={[false, true]}
+            onInputClick={handleInputClick}
+            onInputChange={(name, value) => {
+              if (name === 'input1') setHospitalName(value);
+            }}
+            init={[hospitalName, hospitalAddress]}
+          />
         </div>
-        <FilledTextArea placeholder="메모" />
-
+        <FilledTextArea
+          placeholder="메모"
+          onTextChange={(value) => setHospitalDescription(value)}
+        />
         <h2>병원 방문일</h2>
         <div className="date-input-container">
-          <FilledDateInput type="years" />
-          <FilledDateInput type="months" />
-          <FilledDateInput type="days" />
+          <FilledDateInput
+            type="years"
+            onInput={(value) =>
+              setInputHospitalVisitDate({ ...inputHospitalVisitDate, year: value })
+            }
+          />
+          <FilledDateInput
+            type="months"
+            onInput={(value) =>
+              setInputHospitalVisitDate({ ...inputHospitalVisitDate, month: value })
+            }
+          />
+          <FilledDateInput
+            type="days"
+            onInput={(value) =>
+              setInputHospitalVisitDate({ ...inputHospitalVisitDate, day: value })
+            }
+          />
         </div>
 
         <h2>시간</h2>
         <div className="time-input-container">
-          <FillMeridiemToggle />
-          <FilledTimeInput type="hours" />
-          <FilledTimeInput type="minutes" />
+          <FillMeridiemToggle
+            onChange={(value) =>
+              setInputHospitalVisitTime({ ...inputHospitalVisitTime, meridiem: value })
+            }
+          />
+          <FilledTimeInput
+            type="hours"
+            onInput={(value) =>
+              setInputHospitalVisitTime({ ...inputHospitalVisitTime, hours: value })
+            }
+          />
+          <FilledTimeInput
+            type="minutes"
+            onInput={(value) =>
+              setInputHospitalVisitTime({ ...inputHospitalVisitTime, minutes: value })
+            }
+          />
         </div>
       </div>
       <div className="big-button-wrap">
-        <BigButton>병원 일정 추가하기</BigButton>
+        <BigButton
+          onClick={() => {
+            console.log(
+              hospitalName,
+              hospitalAddress,
+              hospitalDescription,
+              inputHospitalVisitDate,
+              inputHospitalVisitTime,
+              latitude,
+              longitude,
+            );
+          }}
+        >
+          병원 일정 추가하기
+        </BigButton>
       </div>
+      {isPostcodeOpen && (
+        <div className="daum-postcode-wrap" onClick={handlePostcodeWrapClick}>
+          <DaumPostcode className="daum-postcode" autoClose onComplete={handleAddressSelection} />
+        </div>
+      )}
     </div>
   );
 };

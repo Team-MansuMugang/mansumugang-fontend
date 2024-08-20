414e546f1846ca279cc0744561551d15cd240fdb
Dukyoon Im <DUKY8N@gmail.com>
2024-08-21 03:20:34 +0900

    입력폼 유효성 검사 반영

--- src/pages/user-account/ChangeBirthday.jsx
+++ src/pages/user-account/ChangeBirthday.jsx
@@ -1,24 +1,172 @@
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { produce } from 'immer';
 import BigButton from '../../components/BigButton';
 import Input from '../../components/Input';
 import InputWrapper from '../../components/InputWrapper';
 import MainHeader from '../../components/MainHeader';
+import { formatBirthYear, formatBirthMonth, formatBirthDay } from '../utility/inputFormatter';
+import { validBirthYear, validBirthMonth, validBirthDay } from '../utility/accountValidation';
 import './ChangeBirthday.css';
 
 const ChangeBirthday = () => {
+  const [birth, setBirth] = useState({
+    status: 'default',
+    description: '',
+    year: { value: '', status: 'default' },
+    month: { value: '', status: 'default' },
+    day: { value: '', status: 'default' },
+  });
+  const [isAllSuccess, setIsAllSuccess] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (
+      birth.year.status === 'success' &&
+      birth.month.status === 'success' &&
+      birth.day.status === 'success'
+    ) {
+      setIsAllSuccess(true);
+    } else {
+      setIsAllSuccess(false);
+    }
+  }, [birth.year.status, birth.month.status, birth.day.status]);
+
+  const handleBirthYearChange = (event) => {
+    const { status, description } = validBirthYear(event.target.value);
+
+    setBirth((currentBirth) =>
+      produce(currentBirth, (draft) => {
+        draft.year.status = status;
+        draft.status = status;
+        draft.description = description;
+      }),
+    );
+  };
+
+  const handleBirthYearInput = (event) => {
+    event.target.value = formatBirthYear(event.target.value);
+
+    setBirth((currentBirth) =>
+      produce(currentBirth, (draft) => {
+        draft.year.value = event.target.value;
+        draft.day.value = formatBirthDay(birth.year.value, birth.month.value, birth.day.value);
+      }),
+    );
+  };
+
+  const handleBirthMonthChange = (event) => {
+    const { status, description } = validBirthMonth(event.target.value);
+
+    setBirth((currentBirth) =>
+      produce(currentBirth, (draft) => {
+        draft.month.status = status;
+        draft.status = status;
+        draft.description = description;
+      }),
+    );
+  };
+
+  const handleBirthMonthInput = (event) => {
+    event.target.value = formatBirthMonth(event.target.value);
+
+    setBirth((currentBirth) =>
+      produce(currentBirth, (draft) => {
+        draft.month.value = event.target.value;
+        draft.day.value = formatBirthDay(birth.year.value, birth.month.value, birth.day.value);
+      }),
+    );
+  };
+
+  const handleBirthDayChange = (event) => {
+    const { status, description } = validBirthDay(event.target.value);
+
+    setBirth((currentBirth) =>
+      produce(currentBirth, (draft) => {
+        draft.day.status = status;
+        draft.status = status;
+        draft.description = description;
+      }),
+    );
+  };
+
+  const handleBirthDayInput = (event) => {
+    event.target.value = formatBirthDay(birth.year, birth.month, event.target.value);
+
+    setBirth((currentBirth) =>
+      produce(currentBirth, (draft) => {
+        draft.day.value = event.target.value;
+      }),
+    );
+  };
+
+  const handleUpload = async () => {
+    try {
+      // await submitProtectorSignup({
+      //   birthdate: `${birth.year.value}-${birth.month.value.toString().padStart(2, '0')}-${birth.day.value.toString().padStart(2, '0')}`,
+      // });
+
+      try {
+        // toast.info('회원가입이 완료되었습니다. 환영합니다!', { position: 'top-center' });
+        // navigate(-1);
+      } catch (error) {
+        toast.error('오류가 발생했습니다. 잠시 후 다시 시도해주세요', {
+          position: 'bottom-center',
+        });
+      }
+    } catch (error) {
+      toast.warn('입력된 정보들을 확인해주세요', { position: 'top-center' });
+
+      if (error instanceof NotValidRequestError) {
+        error.errorDescriptions.forEach((description) => {
+          if (description.field === 'birthdate') {
+            setBirth((currentId) =>
+              produce(currentId, (draft) => {
+                draft.status = 'warning';
+                draft.description = description.message;
+              }),
+            );
+          }
+        });
+      }
+    }
+  };
 
   return (
     <>
-      <MainHeader title="생년월일 변경하기" onClickLeft={() => navigate(-1)}></MainHeader>
+      <MainHeader title="생년월일 수정하기" onClickLeft={() => navigate(-1)}></MainHeader>
       <div className="change-birthday">
-        <InputWrapper description="생년월일 변경">
-          <Input placeholder="2000" />
-          <Input placeholder="4" />
-          <Input placeholder="27" />
+        <InputWrapper
+          description="생년월일 수정"
+          status={birth.status}
+          statusDescription={birth.description}
+        >
+          <Input
+            placeholder="2000"
+            status={birth.year.status}
+            value={birth.year.value}
+            onChange={handleBirthYearChange}
+            onInput={handleBirthYearInput}
+          />
+          <Input
+            placeholder="4"
+            status={birth.month.status}
+            value={birth.month.value}
+            onChange={handleBirthMonthChange}
+            onInput={handleBirthMonthInput}
+          />
+          <Input
+            placeholder="27"
+            status={birth.day.status}
+            value={birth.day.value}
+            onChange={handleBirthDayChange}
+            onInput={handleBirthDayInput}
+          />
         </InputWrapper>
         <div className="big-button-wrap">
-          <BigButton>수정하기</BigButton>
+          <BigButton disabled={!isAllSuccess} onClick={handleUpload}>
+            수정하기
+          </BigButton>
         </div>
       </div>
     </>
--- src/pages/user-account/ChangeEmail.jsx
+++ src/pages/user-account/ChangeEmail.jsx
@@ -1,23 +1,88 @@
+import { useState, useEffect } from 'react';
+import { produce } from 'immer';
 import { useNavigate } from 'react-router-dom';
 import BigButton from '../../components/BigButton';
 import Input from '../../components/Input';
 import InputWrapper from '../../components/InputWrapper';
 import MainHeader from '../../components/MainHeader';
+import { validateEmail } from '../utility/accountValidation';
 import './ChangeEmail.css';
 
 const ChangeEmail = () => {
+  const [email, setEmail] = useState({ value: '', status: 'default', description: '' });
+  const [isAllSuccess, setIsAllSuccess] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (email.status === 'success') {
+      setIsAllSuccess(true);
+    } else {
+      setIsAllSuccess(false);
+    }
+  }, [email.status]);
+
+  const handleEmailChange = (event) => {
+    const { status, description } = validateEmail(event.target.value);
+
+    setEmail((currentEmail) =>
+      produce(currentEmail, (draft) => {
+        draft.value = event.target.value;
+        draft.status = status;
+        draft.description = description;
+      }),
+    );
+  };
+
+  const handleUpload = async () => {
+    try {
+      // await submitProtectorSignup({
+      //   email: email.value,
+      // });
+
+      try {
+        // toast.info('회원가입이 완료되었습니다. 환영합니다!', { position: 'top-center' });
+        // navigate('/home');
+      } catch (error) {
+        toast.error('오류가 발생했습니다. 잠시 후 다시 시도해주세요', {
+          position: 'bottom-center',
+        });
+      }
+    } catch (error) {
+      toast.warn('입력된 정보들을 확인해주세요', { position: 'top-center' });
+
+      if (error instanceof NotValidRequestError) {
+        error.errorDescriptions.forEach((description) => {
+          if (description.field === 'email') {
+            setEmail((currentId) =>
+              produce(currentId, (draft) => {
+                draft.status = 'warning';
+                draft.description = description.message;
+              }),
+            );
+          }
+        });
+      }
+    }
+  };
 
   return (
     <>
-      <MainHeader title="이메일 변경하기" onClickLeft={() => navigate(-1)}></MainHeader>
+      <MainHeader title="이메일 수정하기" onClickLeft={() => navigate(-1)}></MainHeader>
       <div className="change-email">
-        <InputWrapper description="이메일 변경">
-          <Input placeholder="example@gmail.com" />
+        <InputWrapper
+          description="이메일 수정"
+          status={email.status}
+          statusDescription={email.description}
+        >
+          <Input
+            placeholder="example@gmail.com"
+            onChange={handleEmailChange}
+            status={email.status}
+          />
         </InputWrapper>
-        <div className="big-button-wrap">
-          <BigButton>수정하기</BigButton>
-        </div>
+        <BigButton disabled={!isAllSuccess} onClick={handleUpload}>
+          수정하기
+        </BigButton>
       </div>
     </>
   );

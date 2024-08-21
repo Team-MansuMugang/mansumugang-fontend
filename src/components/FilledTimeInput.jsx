f235d2355edb522fdea0772c1335bf4c81632ae2
Dukyoon Im <DUKY8N@gmail.com>
2024-08-21 14:29:56 +0900

    0 입력 버그 수정

--- src/components/FilledTimeInput.jsx
+++ src/components/FilledTimeInput.jsx
@@ -13,18 +13,33 @@ const FilledTimeInput = ({ init = '', onInput, type = 'hours' }) => {
   useEffect(() => setInput(init), [init]);
 
   const handleInput = (e) => {
-    let value = e.target.value === '' ? '' : parseInt(e.target.value, 10);
+    let value = e.target.value;
+    if (value === '') {
+      setInput('');
+      return;
+    }
 
-    if (type === 'hours' && value !== '') value = Math.max(0, Math.min(value, 12));
-    if (type === 'minutes' && value !== '') value = Math.max(0, Math.min(value, 59));
+    // 숫자 이외의 값이 입력되지 않도록 처리
+    if (/[^0-9]/.test(value)) {
+      return;
+    }
 
-    setInput(value);
-    if (onInput) onInput(value);
+    // 불필요한 0 제거
+    value = value.replace(/^0+/, '');
+    if (value === '') value = '0';
+
+    let numericValue = parseInt(value, 10);
+
+    if (type === 'hours') numericValue = Math.max(0, Math.min(numericValue, 12));
+    if (type === 'minutes') numericValue = Math.max(0, Math.min(numericValue, 59));
+
+    setInput(numericValue.toString());
+    if (onInput) onInput(numericValue);
   };
 
   return (
     <label className="filled-time-input">
-      <input type="number" pattern="\d*" value={input} placeholder="0" onChange={handleInput} />
+      <input type="text" value={input} placeholder="0" onChange={handleInput} />
       <span>{typeText(type)}</span>
     </label>
   );

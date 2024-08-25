bfc1b6466558ad43c041c8bfc4dd8b07561e4e77
HoJin Yu <qaz8520713@naver.com>
2024-08-25 16:04:40 +0900

    음성메세지 클로바 테스트

--- src/apis/api/clovaSpeech.js
+++ src/apis/api/clovaSpeech.js
@@ -5,12 +5,11 @@ export const transcribeVoiceMessage = async (audioUrl) => {
   formData.append('file', audioUrl);
 
   const response = await axios.post(
-    'https://clovaspeech-gw.ncloud.com/external/v1/8889/85b0369195b87877b6f04a5306689d2910e0b0c942cb834c8b67aab0a764bd52/mansumugang/recode-data',
+    'https://clovaspeech-gw.ncloud.com/external/v1/8889/85b0369195b87877b6f04a5306689d2910e0b0c942cb834c8b67aab0a764bd52/http://minnnisu.iptime.org/mm/audio/8e4bfdf549f74d4b96c69226510eb061.mp3',
     formData,
     {
       headers: {
-        'X-NCP-APIGW-API-KEY-ID': process.env.REACT_APP_ACCESS_KEY_ID,
-        'X-NCP-APIGW-API-KEY': process.env.REACT_APP_SECRET_KEY,
+        'X-NCP-APIGW-API-KEY': process.env.local.REACT_APP_CLOVA_SPEECH_API_SECRET,
         'Content-Type': 'application/json',
       },
       params: {

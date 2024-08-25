cb37ca7bdffeb8d74f610c674d489ff099922561
Dukyoon Im <DUKY8N@gmail.com>
2024-08-26 08:37:09 +0900

    개시물 등록시 유효성 확인

--- src/pages/community/NewPostPage.jsx
+++ src/pages/community/NewPostPage.jsx
@@ -1,10 +1,12 @@
 import { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import CategoryHeader from '../../components/CategoryHeader';
 import './NewPostPage.css';
 import PostPictureUpload from '../../components/PostPictureUpload';
 import postCategory from '../../const/postCategory';
 import submitPost from '../../apis/api/submitPost';
+import { ExpiredAccessTokenError, NotValidAccessTokenError } from '../../apis/utility/errors';
 
 const NewPostPage = () => {
   const navigate = useNavigate();
@@ -30,7 +32,7 @@ const NewPostPage = () => {
   const handleTitleChange = (e) => setTitle(e.target.value);
   const handleContentChange = (e) => setContent(e.target.value);
 
-  const submitPostHandler = () => {
+  const submitPostHandler = async () => {
     console.log('제목:', title);
     console.log('내용:', content);
     console.log(
@@ -38,16 +40,53 @@ const NewPostPage = () => {
       Object.keys(postCategory).find((key) => postCategory[key] === selectedCategory),
     );
     console.log('이미지:', images);
-    submitPost(
-      {
-        title,
-        content,
-        categoryCode: Object.keys(postCategory).find(
-          (key) => postCategory[key] === selectedCategory,
-        ),
-      },
-      images,
-    );
+
+    if (!title) {
+      toast.warn('제목을 입력하세요');
+      return;
+    }
+    if (!content) {
+      toast.warn('내용을 입력하세요');
+      return;
+    }
+    if (title.length < 2) {
+      toast.warn('제목은 최소 2글자 이상어야 합니다');
+      return;
+    }
+    if (content.length < 2) {
+      toast.warn('내용은 최소 2글자 이상어야 합니다');
+      return;
+    }
+    if (!selectedCategory) {
+      toast.warn('카테고리를 선택하세요');
+      return;
+    }
+    // toast.warn('병원 이름의 길이는 2에서 20자 사이여야 합니다', { position: 'bottom-center' });
+
+    try {
+      await submitPost(
+        {
+          title,
+          content,
+          categoryCode: Object.keys(postCategory).find(
+            (key) => postCategory[key] === selectedCategory,
+          ),
+        },
+        images,
+      );
+      toast.info('게시물이 작성되었습니다!', { position: 'bottom-center' });
+      navigate(-1);
+    } catch (error) {
+      if (error instanceof ExpiredAccessTokenError) {
+        try {
+          await renewRefreshToken();
+          submitPostHandler();
+        } catch (error) {
+          navigate('/');
+        }
+      } else if (error instanceof NotValidAccessTokenError) navigate('/');
+      else console.error(error);
+    }
   };
 
   const handleImagesChange = (newImages) => {
